7c22bbc426e05edce3fbd8e95ccee7a3a79a79a8
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-04-28 17:35:07 -0500

    Update EmployeeFullName.java

--- EmployeeFullName.java
+++ EmployeeFullName.java
@@ -1,9 +1,15 @@
 abstract class EmployeeFullName{
+	double[] grossPayment,tax,netPayment,bonusPay,startYear;
+	double currentYear=2022;
+	String[] salaryTypes;
 	public String[] firstName;
 	public String[] lastName;
 	public String[] FullName;
 	public int[] employeeID;
 	public abstract void getNumberOfEmployees();
 	public abstract void getData();
+	public abstract void getSalaryType();
+	public abstract void showPayment();
+	public abstract void showEmployeeStartAndBonus();
 }
 
