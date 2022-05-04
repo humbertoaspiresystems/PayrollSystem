c4b66d975a9afb1c79653add500bd77c4f50fdac
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-05-03 19:47:36 -0500

    Update and rename EmployeeFullName.java to EmployeeDetails.java

---
+++ EmployeeDetails.java
@@ -0,0 +1,16 @@
+package com.aspire.employeeDetails;
+
+public class EmployeeDetails {
+	public double[] grossPayment, tax, netPayment, bonusPay;
+	public double currentYear = 2022;
+	public String[] firstName;
+	public String[] lastName;
+	public String[] fullName;
+	public int[] employeeID;
+	public int numberOfEmployees=2,startYear;
+	public void getNumberOfEmployees() {};
+	public void getEmployeeData() {};
+	public void paymentList() {};
+	public void paymentQueue() {};
+	public void displayEmployees() {};
+}
