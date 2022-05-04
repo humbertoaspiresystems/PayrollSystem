309881bf6d6199dd9314506dc0e7273165b3d864
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-05-03 19:46:53 -0500

    Update PayRollSystem.java

--- PayRollSystem.java
+++ PayRollSystem.java
@@ -1,3 +1,7 @@
+package com.aspire.controller;
+
+import com.aspire.employeePayroll.EmployeePayroll;
+
 /* Title: Pay Roll Processing System
  * Author: Jose Humberto Bengochea Aranda
  * Created at:4-16-2022
@@ -10,22 +14,27 @@
  You can extend payroll system class for both employees
  3. Think about interface, how to implement interface concepts and Abstract class
  4. Constructors for application
- Reviewd at:4-27-2022
+ Reviewed at:5-03-2022
  1. Use proper name for exceptions
+ 2. MORE SPECIFIC EXCEPTION CLASSES, AT LAST MORE GENERICA CLASS
+ 3. SPECIFY FOR INPUT
+ 4. IF WE WANT MORE EMPLOYEES, HOW TO ADD THEM UP? ADD EMPLOYEE METHOD, ADD IN MAIN METHOD
+ 5. EXCEPTIONS FOR VALIDATING DATA, NAME AND ID AND SALARY TYPE
+ 6. EXCEPTION FOR START YEARS VALIDATION CHECK STRING/INTS
  */
 public class PayRollSystem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EmployeeInfo employeeInfo = new EmployeeInfo();
-		employeeInfo.getNumberOfEmployees();
-		employeeInfo.getData();
-		employeeInfo.paymentList("Bengochea"); // create exception of there is no one with that last name
-		employeeInfo.paymentList("Wilke");
-		employeeInfo.paymentList("Daniels");
-		employeeInfo.paymentList("Smith");
-		employeeInfo.paymentQueue();
-		//employeeInfo.displayEmployees();
-		//employeeInfo.displayPayment();
-		//employeeInfo.payEmployee();
+		EmployeePayroll employeePayroll = new EmployeePayroll();
+		employeePayroll.getNumberOfEmployees();
+		employeePayroll.getEmployeeData();
+		employeePayroll.paymentList("Bengochea"); // create exception of there is no one with that last name
+		employeePayroll.paymentList("Wilke");
+		employeePayroll.paymentList("Daniels");
+		employeePayroll.paymentList("Smith");
+		employeePayroll.paymentQueue();
+		employeePayroll.displayEmployees();
+		//employeePayroll.displayPayment();
+		//employeePayroll.payEmployee();
 	}
 }
