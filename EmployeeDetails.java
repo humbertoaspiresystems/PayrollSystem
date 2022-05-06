89ea3ab68fcc6bf97ce0c28d2a12c5f0b0886d5f
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-05-06 17:42:28 -0500

    Update EmployeeDetails.java

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -1,16 +1,16 @@
 package com.aspire.employeeDetails;
-
 public class EmployeeDetails {
-	public double[] grossPayment, tax, netPayment, bonusPay;
-	public double currentYear = 2022;
-	public String[] firstName;
-	public String[] lastName;
-	public String[] fullName;
-	public int[] employeeID;
-	public int numberOfEmployees=2,startYear;
+	
+	public String firstName,lastName,salaryType,paymentStatus="";
+	public int employeeID,numberOfEmployees,salary=0,startYear,currentYear=2022;
+	
+	public void connectDatabase() {};
+	public void closeDatabase() {};
+	public void addEmployee(){};
+	public void removeEmployee() {};
+	
+	public void showPayrollSystemStartup() {};
 	public void getNumberOfEmployees() {};
-	public void getEmployeeData() {};
-	public void paymentList() {};
+	public void getEmployeeData() {}
 	public void paymentQueue() {};
-	public void displayEmployees() {};
 }
