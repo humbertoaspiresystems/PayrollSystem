28ab47294fde788bf77c9425b65eafd2be85a746
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-05-10 20:21:38 -0500

    Update EmployeePayroll.java

--- EmployeePayroll.java
+++ EmployeePayroll.java
@@ -10,109 +10,107 @@ import java.util.InputMismatchException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
+import org.apache.logging.log4j.Logger;
+
 import com.aspire.employeeDetails.EmployeeDetails;
-
+import org.apache.logging.log4j.*;
 public class EmployeePayroll extends EmployeeDetails {
-	//EVERYTHING INTO PACKAGE
-	//Log4J
-	//Method for getting the quantity of employees
 	Scanner employeeScanner = new Scanner(System.in);
-	//New coding here
+	static {
+		System.out.println("============================================");
+		System.out.println("  Welcome to Aspire Payroll System App");
+		System.out.println("============================================");}
 	public void showPayrollSystemStartup() {
-		System.out.println("============================================");
-		System.out.println("    Welcome to Aspire Payroll System App");
-		System.out.println("============================================");
-		System.out.println("1.Add Employee");
-		System.out.println("2.Remove Employee");
-		System.out.println("3.Pay Employee");
-		System.out.println("4.Display all employees");
-		System.out.println("5.Display payment queue");
-		System.out.println("6.Exit Payroll System");
-		System.out.println("7.Testing");
-
-		System.out.println("============================================");
+		connectDatabase();
 		int optionSelect=0;
 		while(optionSelect!=6) {
-		System.out.print("Enter option:");
+			waitInput();
+			System.out.println("============================================");
+			System.out.println("  Select an option from the following menu.  ");
+			System.out.println("============================================");
+			System.out.println("1.Add Employee");
+			System.out.println("2.Remove Employee");
+			System.out.println("3.Pay Employee");
+			System.out.println("4.Display all employees");
+			System.out.println("5.Display payment queue");
+			System.out.println("6.Exit Payroll System");
+			System.out.println("============================================");
+			System.out.print("Enter option:");
 		try {
-		optionSelect=employeeScanner.nextInt();}
+		optionSelect=employeeScanner.nextInt();
+		employeeScanner.nextLine();}
 		catch(InputMismatchException inputMismatchException) {
 			System.out.println("Invalid input, please try again.");
 			optionSelect=0;
 			
 		}
 		switch(optionSelect) {
-		case 0: employeeScanner.nextLine();
-				break;
+
 		case 1: addEmployee();
 				break;
+				
 		case 2: removeEmployee();
 				break;
+				
 		case 3: payEmployee();
 				break;
+				
 		case 4: displayEmployees();
 				break;
+				
 		case 5: displayQueue();
 				break;
+				
 		case 6: System.out.println("Exiting Payroll System.");
 				closeDatabase();
 				System.exit(0);
-				break;
+				break;	
+				
 		case 7: testing();
 				break;
-		
+
 		default: System.out.println("Invalid option, please try again.");
 				 break;
 		}}
 	}
-	
-	String user="root";
-	String password="aspire@123";
-	String url="jdbc:mysql://localhost:3306/Employees";
-	Connection connection=null;
-	
-	public void connectDatabase() { 
-		// private connection, STATIC METHOD
-		//Connection to SQL Database
+	private String user="root";
+	private String password="aspire@123";
+	private String url="jdbc:mysql://localhost:3306/Employees";
+	private Connection connection=null;
+	private void connectDatabase(){ 
 				try{ 
-					Class.forName("com.mysql.cj.jdbc.Driver");
 					connection=DriverManager.getConnection(url,user,password);}
-					catch(ClassNotFoundException classNotFound) {
-						classNotFound.printStackTrace();}
 					catch(SQLException sqlException) {
 						sqlException.printStackTrace();}	
-				System.out.println("Connected to Database.");
+				System.out.println("  Connected to Database.");
 	}
-	
-	public void closeDatabase() { // static void too
+	private void closeDatabase() { 
 		try {
 			connection.close();}
-			catch(SQLException e) {
-				e.printStackTrace();}
+			catch(SQLException sqlException) {
+				sqlException.printStackTrace();}
 		System.out.println("Disconnected from database.");
 	}
-	
 	public void addEmployee() {
 		int validateExistence=0;
 		while(validateExistence==0) {
-		employeeScanner.nextLine(); 
-		// no input from the keyboard, MAIN METHOD WITH ARGUMENTS
-		//System.in.(); check on waiting input
-		
+			firstName=null;
+			lastName=null;
+			employeeID=0;
+			salaryType=null;
+			startYear=0;
 		while(firstName==null) {
 			System.out.print("Enter new employee first name:");
 			firstName=employeeScanner.nextLine();
 			if(firstName.matches("^[a-zA-Z]*$")==false) {
 				System.out.println("Wrong input for first name, please try again.");
 				firstName=null;}}
-		
 		while(lastName==null) {
 			System.out.print("Enter new employee last name:");
 			lastName=employeeScanner.nextLine();
 			if(lastName.matches("^[a-zA-Z]*$")==false) {
 				System.out.println("Wrong input for last name, please try again.");
 				lastName=null;}}
-		
 		while(employeeID==0) {
 			try {
 				System.out.print("Enter new employee ID:");
@@ -121,7 +119,6 @@ public class EmployeePayroll extends EmployeeDetails {
 				System.out.println("Wrong input for employee ID, please use a number.");
 				employeeID=0;
 				employeeScanner.nextLine();}}
-			
 		while(salaryType==null) {
 			employeeScanner.nextLine();
 			System.out.print("Enter new salary type (Enter Half or Full Time):");
@@ -131,9 +128,7 @@ public class EmployeePayroll extends EmployeeDetails {
 				salaryType=null;}
 			if(salaryType.equals("FullTime")) {
 				System.out.println("Wrong salary type input, please try again.");
-				salaryType=null;}
-		}
-			
+				salaryType=null;}}	
 		while(startYear==0) {
 		try {
 		System.out.print("Enter new employee starting year:");
@@ -144,62 +139,34 @@ public class EmployeePayroll extends EmployeeDetails {
 		catch(InputMismatchException inputMismatchException) {
 			System.out.println("Input Mismatch Exception, please try again.");}
 		}
-		
 		if(salaryType.equals("Half Time")==true)
-			salary=8500;
-		
+			salary=8500+8500*(currentYear-startYear)*0.05;
 		if(salaryType.equals("Full Time")==true)
-			salary=18500;
-		
+			salary=18500+18500*(currentYear-startYear)*0.07;
 		paymentStatus="Unpaid";
-		
-		//System.out.println(firstName+" "+lastName+" "+employeeID+" "+salaryType+" "+startYear+" "+salary+" "+paymentStatus);
-
 		try {
 		Statement statement=connection.createStatement();
 		String validateEmployeeExistence="SELECT Employee_ID FROM Employees WHERE First_Name='"+firstName+"' AND Last_Name='"+lastName+"'";
 		ResultSet result=statement.executeQuery(validateEmployeeExistence);
-		if(result.next()==true) 
+		if(result.next()==true)
 			System.out.println("Employee already exists in database.");
 		else {
-			String addEmployee="INSERT IGNORE INTO Employees VALUES ('"+firstName+"','"+lastName+"',"+employeeID+",'"+salaryType+"',"+startYear+",'"+salary+"','"+paymentStatus+"')";
-			statement.execute(addEmployee);
-			System.out.println("Employee added to database.");
-			validateExistence=1;}
-		}
-		catch(SQLException sqlException) {
-			sqlException.printStackTrace();}}
-	}
-	public void removeEmployee() {
-		try {
-			int validateExistence=0;
-			employeeScanner.nextLine();
-			while(validateExistence==0) {
-			System.out.print("Enter employee first name:");
-			firstName=employeeScanner.nextLine();
-			System.out.print("Enter employee last name:");
-			lastName=employeeScanner.nextLine();
-			Statement statement=connection.createStatement();
-			String checkEmployeeExists="SELECT Employee_ID FROM Employees WHERE First_Name='"+firstName+"' AND Last_Name='"+lastName+"'";
-			ResultSet result=statement.executeQuery(checkEmployeeExists);
-			if(result.next()==false)
-				System.out.println("Employee does not exist, please try again.");
+			String validateId="SELECT Employee_ID FROM Employees WHERE Employee_ID="+employeeID;
+			ResultSet result2=statement.executeQuery(validateId);
+			if(result2.next()==true) 
+				System.out.println("Employee ID already exists in database.");
 			else {
-				String deleteEmployee="DELETE FROM Employees WHERE First_Name='"+firstName+"' AND Last_Name='"+lastName+"'";
-				statement.execute(deleteEmployee);
-				System.out.println("Employee deleted from database.");
-				validateExistence=1;}
-			}
-			}
-			catch(SQLException sqlException) {
-				sqlException.printStackTrace();}
-	}
-	public void payEmployee() {
-		
+				String addEmployee="INSERT IGNORE INTO Employees VALUES ('"+firstName+"','"+lastName+"',"+employeeID+",'"+salaryType+"',"+startYear+",'"+salary+"','"+paymentStatus+"')";
+				statement.execute(addEmployee);
+				System.out.println("Employee added to database.");
+				validateExistence=1;}}}
+		catch(SQLException sqlException) {
+			sqlException.printStackTrace();}
+		employeeScanner.nextLine();}}
+	public void removeEmployee() {
+		employeeID=0;
 		try {
-			
 			while(employeeID==0) {
-			
 			System.out.print("Enter employee ID:");
 			try {
 			employeeID=employeeScanner.nextInt();}
@@ -214,19 +181,40 @@ public class EmployeePayroll extends EmployeeDetails {
 			if(result.next()==false) {
 				System.out.println("There is no employee with that ID,try again.");
 				employeeID=0;}
-			
+			else {
+				if(employeeID!=0) {
+						String deleteEmployee="DELETE FROM Employees WHERE Employee_ID="+employeeID;						
+						statement.execute(deleteEmployee);
+						System.out.println("Employee deleted from database.");}}}}
+			catch(SQLException sqlException) {
+				sqlException.printStackTrace();}
+	}
+	public void payEmployee() {
+		employeeID=0;
+		try {
+			while(employeeID==0) {
+			System.out.print("Enter employee ID:");
+			try {
+			employeeID=employeeScanner.nextInt();}
+			catch(InputMismatchException inputMismatchException) {
+				System.out.println("Wrong input for ID, please try again.");
+				employeeScanner.nextLine();
+				employeeID=0;
+			}
+			String checkEmployeeExists="SELECT First_Name FROM Employees WHERE Employee_ID="+employeeID;
+			Statement statement=connection.createStatement();
+			ResultSet result=statement.executeQuery(checkEmployeeExists);
+			if(result.next()==false) {
+				System.out.println("There is no employee with that ID,try again.");
+				employeeID=0;}
 			else {
 				if(employeeID!=0) {
 						String payEmployee="UPDATE Employees SET Payment_Status='Paid' WHERE Employee_ID="+employeeID;
 						statement.execute(payEmployee);
 						System.out.println("Employee payment done.");}}}}
-			
 			catch(SQLException sqlException) {
 				sqlException.printStackTrace();}
-			
 	}
-	
-	
 	public void displayEmployees() {
 		try {
 		Statement statement=connection.createStatement();
@@ -234,37 +222,35 @@ public class EmployeePayroll extends EmployeeDetails {
 		ResultSet result=statement.executeQuery(showEmployees);
 		while(result.next()) {
 			String employeeData="";
-			for(int i=1; i<8; i++) {
-			employeeData+=result.getString(i)+"\t";}
+			for(int databaseRowCounter=1; databaseRowCounter<8; databaseRowCounter++) {
+			employeeData+=result.getString(databaseRowCounter)+"\t";}
 			System.out.println(employeeData);
 			}
 		}
 		catch(SQLException sqlException) {
 			sqlException.printStackTrace();}	
 	}
-	
-	public void displayQueue() { //if public-> try and catch, private-> CAN, not necessary throw exceptions
+	public void displayQueue() { 
 		try {
 			Statement statement=connection.createStatement();
 			String displayQueue="SELECT First_Name,Last_Name,Salary FROM Employees WHERE Payment_Status='Unpaid' ORDER BY Start_Year ASC";
 			ResultSet result=statement.executeQuery(displayQueue);
 			while(result.next()) {
 				String employeeData="";
-				for(int i=1; i<4; i++) 
-					employeeData+=result.getString(i)+" ";
+				for(int databaseRowCounter=1; databaseRowCounter<4; databaseRowCounter++) 
+					employeeData+=result.getString(databaseRowCounter)+" ";
 				System.out.println("You owe "+employeeData+"Mexican pesos.");
 				}
 			}
 			catch(SQLException sqlException) {
 				sqlException.printStackTrace();}
 	}
-	
-	public void testing() {
-		//employeeScanner.nextLine();
+	void waitInput() {
+		try {
+			Thread.sleep(2000);} 
+		catch(InterruptedException interruptedException) {
+			interruptedException.printStackTrace();
+		}
 		
-		/*System.out.print("Enter employee first name:");
-		firstName=employeeScanner.nextLine();
-		System.out.print("Enter employee last name:");
-		lastName=employeeScanner.nextLine();*/
 	}
 }
