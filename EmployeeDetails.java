e29e65a12e9774841de88908b87c9570ff48d1c8
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-05-10 20:22:40 -0500

    Update EmployeeDetails.java

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -1,11 +1,12 @@
 package com.aspire.employeeDetails;
-public class EmployeeDetails {
+public class EmployeeDetails extends Thread{
 	
 	public String firstName,lastName,salaryType,paymentStatus="";
-	public int employeeID,numberOfEmployees,salary=0,startYear,currentYear=2022;
+	public int employeeID,numberOfEmployees;
+	public double salary=0,startYear,currentYear=2022;
 	
-	public void connectDatabase() {};
-	public void closeDatabase() {};
+	private void connectDatabase() {};
+	private void closeDatabase() {};
 	public void addEmployee(){};
 	public void removeEmployee() {};
 	
@@ -15,4 +16,5 @@ public class EmployeeDetails {
 	public void paymentQueue() {};
 	
 	public void testing() {};
+	
 }
