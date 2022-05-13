a9c58697632d14b5ec76bafd7998381e40359f79
humbertoaspiresystems <104596861+humbertoaspiresystems@users.noreply.github.com>
2022-05-13 14:58:35 -0500

    Update EmployeeDetails.java

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -1,20 +1,37 @@
 package com.aspire.employeeDetails;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeeDetails extends Thread{
 	
 	public String firstName,lastName,salaryType,paymentStatus="";
-	public int employeeID,numberOfEmployees;
+	public int employeeID;
 	public double salary=0,startYear,currentYear=2022;
-	
-	private void connectDatabase() {};
-	private void closeDatabase() {};
-	public void addEmployee(){};
-	public void removeEmployee() {};
-	
-	public void showPayrollSystemStartup() {};
-	public void getNumberOfEmployees() {};
-	public void getEmployeeData() {}
-	public void paymentQueue() {};
-	
-	public void testing() {};
-	
+	private String user = "root";
+	private String password = "aspire@123";
+	private String url = "jdbc:mysql://localhost:3306/Employees";
+	protected Connection connectionToDatabase = null;
+	protected void connectDatabase() {
+		try {
+			connectionToDatabase = DriverManager.getConnection(url, user, password);
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		System.out.println("  Connected to Database.");
+	};
+	protected void closeDatabase() {
+		try {
+			connectionToDatabase.close();
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		System.out.println("Disconnected from database.");
+	};
+	public void waitForInput() {
+		try {
+			Thread.sleep(2000);}
+		catch (InterruptedException interruptedException) {
+			interruptedException.printStackTrace();}}
 }
